1915197128b9446aeb467cc3dd391d15a11068cc
Ben Frisbie <bbfrisbi@gmail.com>
2020-12-06 15:17:27 -0500

    -new bootstrap -added 404 page -tidy things up

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/benfrisbie/benfrisbie-web
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-contrib/multitemplate v0.0.0-20200916052041-666a7309d230
+	github.com/gin-gonic/gin v1.6.3
+)
--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello world!")
+	renderer := multitemplate.NewRenderer()
+	renderer.AddFromFiles("index", "assets/templates/base.html", "assets/templates/index.html")
+	renderer.AddFromFiles("404", "assets/templates/base.html", "assets/templates/404.html")
+	router.HTMLRender = renderer
+	router.Static("/static", "./assets/static")
+
+	// Home page
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index", gin.H{})
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	// 404 not found page
+	router.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusOK, "404", gin.H{})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
